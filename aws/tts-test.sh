98f48ca91e2371a7404e60e40d0833eedb9a8660
hoseog.lee <hoseog@gmail.com>
2019-12-23 14:31:02 +0900

    update

--- aws/tts-test.sh
+++ aws/tts-test.sh
@@ -1,1 +1,1 @@
-aws polly synthesize-speech --output-format mp3 --voice-id Joanna --text "Hello my name is Joanna." joanna.mp3
+aws polly synthesize-speech --output-format mp3 --voice-id Joanna --text "Hello Jihey, my name is Jarvis." joanna.mp3
