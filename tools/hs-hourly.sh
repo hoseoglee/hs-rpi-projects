d3888b55082327594c1f7ec859eeafe338e1f28c
hoseoglee <hoseog@gmail.com>
2020-01-10 10:23:49 +0900

    update

--- tools/hs-hourly.sh
+++ tools/hs-hourly.sh
@@ -1,4 +1,4 @@
-sudo cp /home/pi/hs-rpi-projects/rpi-tool/hs-hourly.sh /etc/cron.hourly/hs-hourly.sh
+sudo cp /home/pi/hs-rpi-projects/tools/hs-hourly.sh /etc/cron.hourly/hs-hourly.sh
 
-/home/pi/hs-rpi-projects/rpi-tool/get_my_ip.sh
-/home/pi/hs-rpi-projects/rpi-tool/auto-push.sh
+/home/pi/hs-rpi-projects/tools/get_my_ip.sh
+/home/pi/hs-rpi-projects/tools/auto-push.sh
