fb50425717e46cf5bd2b49037fd1c505bf0f5357
hoseoglee <hoseog@gmail.com>
2020-01-14 18:00:02 +0900

    update

--- opencv/capture.py
+++ opencv/capture.py
@@ -1,45 +1,18 @@
 import cv2 
 
-key = cv2. waitKey(1)
+path="/home/pi/jarvis/public/img/"
+
 webcam = cv2.VideoCapture(0)
-while True:
-    try:
-        check, frame = webcam.read()
-        print(check) #prints true as long as the webcam is running
-        print(frame) #prints matrix values of each framecd 
-        #cv2.imshow("Capturing", frame)
-        key = cv2.waitKey(1)
-        #if key == ord('s'): 
-        cv2.imwrite(filename='saved_img.jpg', img=frame)
-        webcam.release()
-        img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
-        img_new = cv2.imshow("Captured Image", img_new)
-        cv2.waitKey(1650)
-        cv2.destroyAllWindows()
-        print("Processing image...")
-        img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
-        print("Converting RGB image to grayscale...")
-        gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-        print("Converted RGB image to grayscale...")
-        print("Resizing image to 28x28 scale...")
-        img_ = cv2.resize(gray,(28,28))
-        print("Resized...")
-        img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
-        print("Image saved!")
-        
-        break
-       # elif key == ord('q'):
-       #     print("Turning off camera.")
-       #     webcam.release()
-       #     print("Camera off.")
-       #     print("Program ended.")
-       #     cv2.destroyAllWindows()
-       #     break
-        
-    except(KeyboardInterrupt):
-        print("Turning off camera.")
-        webcam.release()
-        print("Camera off.")
-        print("Program ended.")
-        cv2.destroyAllWindows()
-        break
+check, frame = webcam.read()
+print(check)
+print(frame)
+cv2.imwrite(filename=path+'img1.jpg', img=frame)
+webcam.release()
+
+
+webcam2 = cv2.VideoCapture(2)
+check, frame = webcam2.read()
+print(check)
+print(frame)
+cv2.imwrite(filename=path+'img2.jpg', img=frame)
+webcam2.release()
