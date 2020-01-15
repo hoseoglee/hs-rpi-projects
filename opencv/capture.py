8e8001aa91c29b3407b7a04bed343dedae601e32
hoseoglee <hoseog@gmail.com>
2020-01-15 18:00:02 +0900

    update

--- opencv/capture.py
+++ opencv/capture.py
@@ -3,12 +3,11 @@ import cv2
 path="/home/pi/jarvis/public/img/"
 
 cam1 = cv2.VideoCapture(0)
-cam2 = cv2.VideoCapture(2)
-
 check1, frame1 = cam1.read()
 cv2.imwrite(filename=path+'img1.jpg', img=frame1)
 cam1.release()
 
+cam2 = cv2.VideoCapture(2)
 check2, frame2 = cam2.read()
 cv2.imwrite(filename=path+'img2.jpg', img=frame2)
 cam2.release()
