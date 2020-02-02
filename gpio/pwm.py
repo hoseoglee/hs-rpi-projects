1027db40871ea7c5681fd66bda1bd9cc7460ceec
hoseoglee <hoseog@gmail.com>
2020-02-02 14:00:01 +0900

    update

--- gpio/pwm.py
+++ gpio/pwm.py
@@ -1,9 +1,8 @@
 import RPi.GPIO as GPIO  
 import time  
 
-GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-#GPIO.setmode( GPIO.BOARD )  
+GPIO.setmode( GPIO.BOARD )  
   
 #12번핀은 출력모드로 설정  
 GPIO.setup(12, GPIO.OUT)  
@@ -11,17 +10,17 @@ GPIO.setup(12, GPIO.OUT)
 
 #GPIO.output(17,GPIO.HIGH)  
 
-pwm = GPIO.PWM(12,5) #50hz  
+pwm = GPIO.PWM(12,50) #50hz  
 pwm.start(0)  
   
 for i in range(0,3):  
   
-    for dc in range(0,101,200):  
+    for dc in range(0,101,5):  
         pwm.ChangeDutyCycle(dc)  
         time.sleep(0.1)  
-    for dc in range(100,-1,-5):
-        pwm.ChangeDutyCycle(dc)
-        time.sleep(0.1)
+    for dc in range(100,-1,-5):  
+        pwm.ChangeDutyCycle(dc)  
+        time.sleep(0.1)  
   
 pwm.stop()  
 GPIO.cleanup()
